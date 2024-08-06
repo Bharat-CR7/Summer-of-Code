6c610eeddb3596bceaa626e8b538db4e74bc2cc1
Bharat Arora <168494047+Bharat-CR7@users.noreply.github.com>
2024-08-06 18:24:02 +0530

    Create My agent.py

---
+++ Week 7/My agent.py
@@ -0,0 +1,19 @@
+def my_agent(obs, config):
+    # Your code here: Amend the agent!
+    import numpy as np
+    import random
+
+    valid_moves = [col for col in range(config.columns) if obs.board[col] == 0]
+    for col in valid_moves:
+        if check_winning_move(obs, config, col, obs.mark):
+            return col
+    for col in valid_moves:
+        if check_winning_move(obs, config, col, obs.mark%2+1):
+            return col
+    return 0 
+    
+    
+    
+    import random
+    valid_moves = [col for col in range(config.columns) if obs.board[col] == 0]
+    return random.choice(valid_moves)
